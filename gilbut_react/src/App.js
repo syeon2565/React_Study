8bda151a477bafe0a93c7a2669d32d8874bafb4f
seongyeon <ysy2565@naver.com>
2021-10-06 23:18:11 +0900

    book 3.1

---
+++ gilbut_react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component{
+  render(){
+  const name = '리액트';
+  return 
+    <div className="App">
+     {name}
+    </div>
+  }
+}
+
+export default App;
