bb203d7340b604d4f4d793ab0a720c4f9d5e74a2
seongyeon <ysy2565@naver.com>
2021-10-07 23:37:21 +0900

    props example

--- gilbut_react/src/App.js
+++ gilbut_react/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import React from 'react';
 import MyComponent from './MyComponent';
 
-// const App = () =>{
-//   return <MyComponent/>
-// }
-const App = () => <MyComponent/>
+const App = () =>{
+  return <MyComponent />
+}
+// const App = () => <MyComponent/> 
+//위와 아래는 같은 의미를 나타냄.
 export default App;
