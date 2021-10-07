2967aafdd127db7273cf2b5d75e2291d7ae9d5f0
seongyeon <ysy2565@naver.com>
2021-10-08 00:19:38 +0900

    state

--- gilbut_react/src/App.js
+++ gilbut_react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 // import { Component } from 'react';
 import React from 'react';
-import MyComponent from './MyComponent';
+// import MyComponent from './MyComponent';
+import Counter from './Counter';
+
 
 const App = () =>{
-  return <MyComponent />
+  return <Counter/>
 }
 // const App = () => <MyComponent/> 
 //위와 아래는 같은 의미를 나타냄.
