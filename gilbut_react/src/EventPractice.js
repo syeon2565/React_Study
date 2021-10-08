4a3e3869b27e9e49b12cec5de89b6a398452b222
seongyeon <ysy2565@naver.com>
2021-10-09 01:31:14 +0900

    function_useState_keyPress

--- gilbut_react/src/EventPractice.js
+++ gilbut_react/src/EventPractice.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
 
 const EventPractice = () => {
-  const [username, setUsername] = useState("");
-  const [message, setMessage] = useState("");
-  const onChangeUsername = (e) => setUsername(e.target.value);
-  const onChangeMessage = (e) => setMessage(e.target.value);
-  const onClick = () => {
-    alert(username + ": " + message);
-    setUsername("");
-    setMessage("");
+  const [form, setForm] = useState({
+    username:'',
+    message:'',
+  });
+  const {username, message} = form;
+  const onChange = e =>{
+    const nextForm = {
+      ...form,
+      [e.target.name]:e.target.value
+    };
+    setForm(nextForm);
   };
+  const onClick=()=>{
+    alert(username + ': ' + message);
+    setForm({
+      username:'',
+      message:''
+    });
+  };
+
   const onKeyPress = e => {
     if (e.key === "Enter") {
       onClick();
@@ -23,14 +34,14 @@ const EventPractice = () => {
         name="username"
         placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       ></input>
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={{ onKeyPress }}
       ></input>
       <button onClick={onClick}>확인</button>
