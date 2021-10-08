924cba6dacac0e0d097c62ff37a77b559094c6ff
seongyeon <ysy2565@naver.com>
2021-10-08 23:15:10 +0900

    useState

--- gilbut_react/src/App.js
+++ gilbut_react/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 // import { Component } from 'react';
 import React from 'react';
 // import MyComponent from './MyComponent';
-import Counter from './Counter';
+import Say from './Say';
 
 
 const App = () =>{
-  return <Counter/>
+  return <Say/>
 }
 // const App = () => <MyComponent/> 
 //위와 아래는 같은 의미를 나타냄.
--- gilbut_react/src/Counter.js
+++ gilbut_react/src/Counter.js
@@ -3,25 +3,26 @@ import { Component } from "react";
 
 class Counter extends Component {
   state = {
-      number: 0,
-      fixednumber: 0,
-    };
+    number: 0,
+    fixednumber: 0,
+  };
   render() {
-    const { number,fixednumber } = this.state;
+    const { number, fixednumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
         <h2>바뀌지 않는 값 : {fixednumber}</h2>
         <button
           onClick={() => {
-            this.setState(prevState=>{
-              return {
-                number: prevState.number + 1
-              };
-            });
-            this.setState(prevState=>({
-              number:prevState.number+1
-            }))
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log("방금 setState호출");
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
