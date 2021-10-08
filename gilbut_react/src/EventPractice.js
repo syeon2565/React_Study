da19c000d90aa177505dcdc349232729db10e812
seongyeon <ysy2565@naver.com>
2021-10-09 00:43:08 +0900

    class_keypress

--- gilbut_react/src/EventPractice.js
+++ gilbut_react/src/EventPractice.js
@@ -2,23 +2,25 @@ import React, { Component } from "react";
 
 class EventPractice extends Component {
   state = {
+    username: "",
     message: "",
   };
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
-      message: e.target.value,
+      [e.target.name]: e.target.value,
     });
   }
-  handleClick() {
-    alert(this.state.message);
+  handleClick = () => {
+    alert(this.state.username + ": " + this.state.message);
     this.setState({
+      username: "",
       message: "",
     });
+  }
+  handleKeyPress = (e) => {
+    if(e.key === 'Enter'){
+      this.handleClick();
+    }
   }
   render() {
     return (
@@ -26,15 +28,20 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
-        <button
-          onClick={this.handleClick}>
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
