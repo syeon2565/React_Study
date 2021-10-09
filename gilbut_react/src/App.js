94593cba9a0fd8f7f8e6e8c2bd48813753e1ed2c
seongyeon <ysy2565@naver.com>
2021-10-10 00:07:57 +0900

    useEffect

--- gilbut_react/src/App.js
+++ gilbut_react/src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
-import React from "react";
-import Counter from "./Counter";
+import React, { useState } from "react";
+import Info from "./Info";
 
-const App = () =>{
-  return <Counter/>
-}
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
+};
 export default App;
