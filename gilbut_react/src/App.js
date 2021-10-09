f4975ba4ded92392fc20e44941aa0c90d5001626
seongyeon <ysy2565@naver.com>
2021-10-09 21:04:40 +0900

    LifeCycleSample AND ErrorBoundary

--- gilbut_react/src/App.js
+++ gilbut_react/src/App.js
@@ -1,16 +1,31 @@
 import "./App.css";
 import { Component } from "react";
 import React from "react";
-// import MyComponent from './MyComponent';
-import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+  handleQuick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
   render() {
     return (
       <div>
-        <IterationSample/>
-        
-      </div>)
+        <button onClick={this.handleQuick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 // const App = () => <MyComponent/>
