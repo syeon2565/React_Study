bf8bf09515fdac9f7c4a1fb7b59dd74d14c8b397
seongyeon <ysy2565@naver.com>
2021-10-09 23:18:32 +0900

    useState Counter.js

--- gilbut_react/src/App.js
+++ gilbut_react/src/App.js
@@ -1,33 +1,8 @@
 import "./App.css";
-import { Component } from "react";
 import React from "react";
-import LifeCycleSample from "./LifeCycleSample";
-import ErrorBoundary from "./ErrorBoundary";
+import Counter from "./Counter";
 
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+const App = () =>{
+  return <Counter/>
 }
-
-class App extends Component {
-  state = {
-    color: "#000000",
-  };
-  handleQuick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleQuick}>랜덤색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
-// const App = () => <MyComponent/>
-//위와 아래는 같은 의미를 나타냄.
 export default App;
--- gilbut_react/src/Counter.js
+++ gilbut_react/src/Counter.js
@@ -1,35 +1,15 @@
-import React from "react";
-import { Component } from "react";
+import React, { useState } from "react";
 
-class Counter extends Component {
-  state = {
-    number: 0,
-    fixednumber: 0,
-  };
-  render() {
-    const { number, fixednumber } = this.state;
-    return (
-      <div>
-        <h1>{number}</h1>
-        <h2>바뀌지 않는 값 : {fixednumber}</h2>
-        <button
-          onClick={() => {
-            this.setState(
-              {
-                number: number + 1,
-              },
-              () => {
-                console.log("방금 setState호출");
-                console.log(this.state);
-              }
-            );
-          }}
-        >
-          +1
-        </button>
-      </div>
-    );
-  }
-}
-
+const Counter = () => {
+  const [value, setValue] = useState(0);
+  return (
+    <div>
+      <p>
+        현재 카운터 값은 <b>{value}</b> 입니다.
+      </p>
+      <button onClick={() => setValue(value + 1)}>+1</button>
+      <button onClick={() => setValue(value - 1)}>-1</button>
+    </div>
+  );
+};
 export default Counter;
