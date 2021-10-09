2d356ebdc965a90f60afcf1c8f9a6bb5ea35664d
seongyeon <ysy2565@naver.com>
2021-10-10 00:53:38 +0900

    useReducer

--- gilbut_react/src/Info.js
+++ gilbut_react/src/Info.js
@@ -1,35 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-  useEffect(()=>{
-    console.log('effect');
-    console.log(name);
-    return()=>{
-      console.log('cleanup');
-      console.log(name)
-    }
-  }, [name]);
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
+  };
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name"value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
-          <b>이름</b> {name}
+          <b>이름: </b> {name}
         </div>
         <div>
-          <b>닉네임</b> {nickname}
+          <b>닉네임: </b> {nickname}
         </div>
       </div>
     </div>
