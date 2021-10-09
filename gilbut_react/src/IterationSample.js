c2d202ba69aa15465c9dd1240ffec4ce66b9de89
seongyeon <ysy2565@naver.com>
2021-10-09 18:32:07 +0900

    doubleClick_date delete

--- gilbut_react/src/IterationSample.js
+++ gilbut_react/src/IterationSample.js
@@ -20,7 +20,12 @@ const IterationSample = () => {
     setNames(nextNames);
     setInputText("");
   };
-  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+  const nameList = names.map((name) => <li key={name.id} onDoubleClick={()=>onRemove(name.id)}>
+    {name.text}</li>);
   return (
     <>
       <input value={inputText} onChange={onChange} />
