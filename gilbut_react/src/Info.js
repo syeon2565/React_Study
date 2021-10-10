33331a2c873fdfb163d1f04ab4008d237885d8c9
seongyeon <ysy2565@naver.com>
2021-10-10 14:36:08 +0900

    custom_hook

--- gilbut_react/src/Info.js
+++ gilbut_react/src/Info.js
@@ -1,26 +1,18 @@
-import React, { useReducer } from "react";
-
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value,
-  };
-}
+import React from "react";
+import useInputs from "./useInputs";
 
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs( {
     name: "",
     nickname: "",
   });
   const { name, nickname } = state;
-  const onChange = (e) => {
-    dispatch(e.target);
-  };
+
 
   return (
     <div>
       <div>
-        <input name="name"value={name} onChange={onChange} />
+        <input name="name" value={name} onChange={onChange} />
         <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
