1ebb65e2bdf48c960ca6b5874facd62187626af4
seongyeon <ysy2565@naver.com>
2021-10-14 23:48:50 +0900

    axios_text

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -3,16 +3,11 @@ import axios from 'axios';
 
 const App =()=>{
   const [data, setData] = useState(null);
-  const onClick = async () =>{
-    try{
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
-      );
+  const onClick =() =>{
+    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response=>{
       setData(response.data);
-    } catch(e){
-      console.log(e);
-    }
-  }
+    });
+  };
   return(
     <div>
       <div>
