88c0813649cc59f8b5b8ac7aea9fb7f258dfd40f
seongyeon <ysy2565@naver.com>
2021-10-17 22:10:53 +0900

    bindActionCreators no

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -14,8 +14,8 @@ export default connect(
   state=>({
     number:state.counter.number,
   }),
-  dispatch=>({
-    increase:()=>dispatch(increase()),
-    decrease:()=>dispatch(decrease()),
-  }),
+      {
+        increase,
+        decrease,
+      },
 )(CounterContainer);
