658e23edb75d1b8280b68b79cb10607872cc19c5
seongyeon <ysy2565@naver.com>
2021-10-17 21:45:34 +0900

    451p connet 익명함수

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Todos from './components/Todos';
-// import CounterContainer from './containers/CounterContainer';
-import Counter from './components/Counter';
+import CounterContainer from './containers/CounterContainer';
+// import Counter from './components/Counter';
 const App = () => {
   return (
     <div>
-      <Counter number={0} />
+      <CounterContainer/>
       <hr />
       <Todos />
     </div>
