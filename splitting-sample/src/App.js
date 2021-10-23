bfc7553358699f2d602e8333108646dbb7bfa7d7
seongyeon <ysy2565@naver.com>
2021-10-23 18:53:43 +0900

    code splitting

---
+++ splitting-sample/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import notify from './notify';
+
+function App() {
+  const onClick = ()=>{
+    notify();
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}> Hello React1!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
