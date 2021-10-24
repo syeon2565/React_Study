05adaaf82793d5f563b7caa8cf89656e95fbe813
seongyeon <ysy2565@naver.com>
2021-10-24 16:11:57 +0900

    20.3.3 build script

--- ssr-recipe/scripts/build.server.js
+++ ssr-recipe/scripts/build.server.js
@@ -1,5 +1,5 @@
 process.env.BABEL_ENV = 'production';
-process.nextTick.NODE_ENV = 'production';
+process.env.NODE_ENV = 'production';
 
 process.on('unhandledRejection', err => {
   throw err;
@@ -11,13 +11,13 @@ const webpack = require('webpack');
 const config = require('../config/webpack.config.server');
 const paths = require('../config/paths');
 
-function build(){
-  console.log('Creating server build');
+function build() {
+  console.log('Creating server build...');
   fs.emptyDirSync(paths.ssrBuild);
   let compiler = webpack(config);
-  return new Promise((resolve, reject)=>{
-    compiler.run((err,stats)=>{
-      if(err){
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err) {
         console.log(err);
         return;
       }
