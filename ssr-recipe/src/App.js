a25503d82069accb43103d61a752d4868dbff71d
seongyeon <ysy2565@naver.com>
2021-10-24 13:56:00 +0900

    project ready

---
+++ ssr-recipe/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route } from "react-router";
+import Menu from "./components/Menu";
+import RedPage from "./pages/RedPage";
+import BluePage from "./pages/BluePage";
+
+const App=()=>{
+  return(
+    <div>
+      <Menu/>
+      <hr/>
+      <Route path="/red" component={RedPage}/>
+      <Route path="/blue" component={BluePage}/>
+    </div>
+  )
+}
+export default App;
