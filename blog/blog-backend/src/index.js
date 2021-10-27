d90caebc96578150b2d3860622fec48d60593481
seongyeon <ysy2565@naver.com>
2021-10-27 19:38:57 +0900

    21.5.2 koa-router

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,26 +1,23 @@
 const Koa = require('koa');
+const Router = require('koa-router')
 
 const app = new Koa();
+const router = new Router()
 
-app.use(async(ctx,next)=>{
-    console.log(ctx.url);
-    console.log(1);
-    if(ctx.query.authorized !=='1'){
-        ctx.status = 401;
-        return;
-    }
-    await next();
-    console.log('END');
-});
-app.use((ctx,next)=>{
-    console.log(2);
-    next();
+router.get('/', ctx => {
+    ctx.body = '홈';
 });
 
-app.use(ctx=>{
-    ctx.body = "hello world";
-});
+router.get('/about/:name?', ctx=>{
+    const {name} = ctx.params;
+    ctx.body = name ? `${name}의 소개` : '소개';
+})
+router.get('/posts', ctx=>{
+    const {id} = ctx.query;
+    ctx.body=id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+})
+app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log('Listening to port 4000');
 });
