080bd1cc1d8b60282ffe4c3d1dcdddcc9d2d60d7
seongyeon <ysy2565@naver.com>
2021-10-28 21:37:28 +0900

    mongodb

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-undef */
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser');
+
+const {PORT} = process.env;
 
 const api = require('./api');
 const app = new Koa();
@@ -10,6 +14,7 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-    console.log('Listening to port 4000');
-});
+const port = PORT || 4000;
+app.listen(port, ()=>{
+    console.log('Listeninng to port %d', port)
+} )
