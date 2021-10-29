ec2d2c89341fc21ecc44a9f2912ff6deb9f44c69
seongyeon <ysy2565@naver.com>
2021-10-30 01:42:41 +0900

    mongo test

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,29 +1,40 @@
-/* eslint-disable no-undef */
-require('dotenv').config();
+require('dotenv').config()
 const Koa = require('koa');
-const Router = require('koa-router')
+const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
-const mongoose = requires('mongoose');
+const mongoose = require('mongoose');
 
-const api = require('./api');
-const {PORT, MONGO_URI} = process.env;
+//비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기 
+// eslint-disable-next-line no-undef
+const { PORT } = process.env;
 
 mongoose
-.connect(MONGO_URI, {useNewUrlParse:true, useFindAndModify:false})
-    .then(()=>{
-        console.log('Connected to MongoDB');
+    .connect("mongodb://localhost:27017/blog", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('connected to MongoDB');
     })
-    .catch(e=>{
-        console.error(e);
+    .catch(e => {
+        console.error(e)
     })
-const app = new Koa();
-const router = new Router()
 
+
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
+
+//라우터 설정 
 router.use('/api', api.routes());
+
+// 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
+//[PST가 지정되어 있지 않다면 4000을 사용 
 const port = PORT || 4000;
-app.listen(port, ()=>{
-    console.log('Listeninng to port %d', port)
-} )
+
+app.listen(port, () => {
+    console.log('Listening to port %d', port);
+})
