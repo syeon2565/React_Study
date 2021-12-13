783b7a33a9d14d5b3d6e310d1dd676b6dceae940
seongyeon <ysy2565@naver.com>
2021-12-13 23:28:59 +0900

    modify error

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,5 +1,5 @@
-const Router = require('koa-router');
-const posts = require('./posts');
+import Router from 'koa-router';
+import posts from './posts';
 
 const api = new Router();
 
--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -3,7 +3,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+// import api from './api/index';
 import api from './api';
+
 //비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
 // eslint-disable-next-line no-undef
 const { PORT, MONGO_URI } = process.env;
