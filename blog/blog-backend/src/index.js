08dce03cf77284d54280eea55448a1647f3b6646
seongyeon <ysy2565@naver.com>
2021-12-14 21:18:40 +0900

    route api error solve

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -17,13 +17,13 @@ mongoose
     console.error(e);
   });
 
-const api = require('./api');
+// const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
 
 //라우터 설정
-router.use('/api', api.routes());
+// router.use('/api', api.routes());
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
