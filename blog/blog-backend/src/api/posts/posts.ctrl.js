989100eb770ccc4aac70706ec43b88f67a8eb26a
seongyeon <ysy2565@naver.com>
2021-12-19 00:02:31 +0900

    page function

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -43,10 +43,17 @@ export const write = async ctx => {
 };
 
 export const list = async ctx => {
+    const page = parseInt(ctx.query.page || '1', 10);
+
+    if (page <1){
+        ctx.status = 400;
+        return;
+    }
     try {
         const posts = await Post.find()
             .sort({ _id: -1 })
             .limit(10)
+            .skip((page-1) * 10)
             .exec();
         
         ctx.body = posts;
